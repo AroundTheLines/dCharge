2cb3a71e15ee87bacdc69f19d3eaf02ae4e690a2
Ethan Liang <ehliang@uwaterloo.ca>
2016-04-23 21:00:26 +0200

    Communication between watch and phone, send data to server complete

---
+++ Android/Dcharge3/mobile/src/main/java/nioth/dev/dcharge/ListenerService.java
@@ -0,0 +1,32 @@
+package nioth.dev.dcharge;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class ListenerService extends WearableListenerService {
+
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+
+        if (messageEvent.getPath().equals("/heart-monitor")) {
+            final String message = new String(messageEvent.getData());
+            Log.i("myTag", "Message path received on watch is: " + messageEvent.getPath());
+            Log.i("myTag", "Message received on watch is: " + message);
+
+            Intent messageIntent = new Intent();
+            messageIntent.setAction(Intent.ACTION_SEND);
+            messageIntent.putExtra("message", message);
+            LocalBroadcastManager.getInstance(this).sendBroadcast(messageIntent);
+        }
+        else {
+            super.onMessageReceived(messageEvent);
+        }
+    }
+}

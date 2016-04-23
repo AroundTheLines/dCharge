edad9f72cb8b33a1ac56911e2d67b04d12026625
Umesh Kumar <umeshkumarkhanna@hotmail.com>
2016-04-23 13:16:49 +0200

    updated buttons android

--- Android/Dcharge3/mobile/src/main/java/nioth/dev/dcharge/LoginActivity.java
+++ Android/Dcharge3/mobile/src/main/java/nioth/dev/dcharge/LoginActivity.java
@@ -1,16 +1,26 @@
 package nioth.dev.dcharge;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, TutorialActivity.class);
+                 startActivity(intent);
+             }
+        });
     }
 
     @Override

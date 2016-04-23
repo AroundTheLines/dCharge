6f3f71158d38e42561e10be1e52a1b94c4b8acfe
Umesh Kumar <umeshkumarkhanna@hotmail.com>
2016-04-23 07:26:09 +0200

    added home activity

--- Android/Dcharge3/mobile/src/main/java/nioth/dev/dcharge/HomeActivity.java
+++ Android/Dcharge3/mobile/src/main/java/nioth/dev/dcharge/HomeActivity.java
@@ -1,16 +1,68 @@
 package nioth.dev.dcharge;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+    View heartRateButton;
+    View responseHistoryButotn;
+    View hospitalInfoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        heartRateButton = findViewById(R.id.heart_rate_history_button);
+        responseHistoryButotn = findViewById(R.id.response_history_button);
+        hospitalInfoButton = findViewById(R.id.hospital_info);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
+        heartRateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+
+        responseHistoryButotn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Context context = getApplicationContext();
+                CharSequence text = "Response!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        hospitalInfoButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Context context = getApplicationContext();
+                CharSequence text = "Hospital!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 
     @Override
--- Android/Dcharge3/mobile/src/main/java/nioth/dev/dcharge/TutorialActivity.java
+++ Android/Dcharge3/mobile/src/main/java/nioth/dev/dcharge/TutorialActivity.java
@@ -2,6 +2,7 @@ package nioth.dev.dcharge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +12,8 @@ public class TutorialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 
     @Override

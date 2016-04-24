400b71dbd3d8ab2bdc3c8c0697c0582643810513
Umesh Kumar <umeshkumarkhanna@hotmail.com>
2016-04-24 09:52:11 +0200

    changed questions

--- Android/Dcharge3/mobile/src/main/java/nioth/dev/dcharge/PastQuestions/MyData.java
+++ Android/Dcharge3/mobile/src/main/java/nioth/dev/dcharge/PastQuestions/MyData.java
@@ -6,7 +6,7 @@ package nioth.dev.dcharge.PastQuestions;
 public class MyData {
 //DATA OF ALL QUESTIONS AND THEIR ANSWERS
 
-    static String[] questionsArray = {"Are you ok?", "Hod do you feel?","Are you ok?", "Hod do you feel?","Are you ok?", "Hod do you feel?","Are you ok?", "Hod do you feel?","Are you ok?", "Hod do you feel?",};
+    static String[] questionsArray = {"Do you need any assistance right now? ", "Do you feel pain?","Do you have diarrhea or constipation?", "Do you feel tired or fatigue?","Do you have any visible skin irritations", "Have you been able to eat well?","Do you feel tingling or numbness anywhere", "Do you have diarrhea or constipation","Do you feel tired or fatigue", "Have you been able to eat well?",};
     static String[] timeArray = {"10m", "10m", "11m", "1h20m", "1h19m", "1h19m", "2h3m", "2h3m", "2h2m", "3h21m"};
     static String[] answersArray = {"YES", "NO", "YES", "YES", "NO", "YES", "NO", "YES", "YES", "NO"};
     static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
